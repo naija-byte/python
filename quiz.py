b4c98614d2954a1378715fb3f6a54f6ef69a2cd4
naija-byte <sukhijanaija@gmail.com>
2025-11-07 13:38:50 +0530

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,72 @@
+#storytime woohoo
+
+print("====== Your own Adventure Game ====== ")
+print()
+print()
+print("Get ready to have some fun BUT you need to tell us about yourself!")
+print()
+print()
+char = input("""What type of character are you?
+a) Protector of the forest
+b) Angel/Spirit
+c) Magician
+d) Genius Scientist
+""")
+print()
+name = input("What's the name of your character? ")
+print()
+animal = input("""Which animal are you accompanied by on your adventures?
+a) Giant eagle
+b) Race Horse
+c) Powerful Dog
+d) Sly fox
+""")
+print()
+enemy = input("What is your worst enemy's name? ")
+print()
+villain = input("""Which villain do you want?
+a) Hunter/Animal Killer
+b) Ghost
+c) Vampire
+d) Alien Monster
+""")
+print()
+superpower = input("""Which superpower do you want in the game?
+a) Invisibility
+b) Stopping time
+c) Reading minds
+d) Going back in time
+""")
+print()
+live = input("Do you live in a castle, forest, underwater or a magical land? ")
+print()
+weapon = input("Which weapon do you use? ")
+print()
+cape = input("Which color cape do you have? ")
+print()
+food = input("What do you like to eat? ")
+print()
+print(
+    "================ Your Adventure Game is ready and completely personalised!=============="
+)
+print()
+print()
+print("Once upon a time, in a far and distant land, there lived a ", char,
+      "who was called ", name)
+print()
+print("She lived in ", live, "constantly eating", food,
+      "because that was her favorite")
+print("she had a", animal, "as a pet who accompanied her everywhere she went")
+print()
+print(
+    "One randon day,", name,
+    "was roaming around her garden trying to collect leaves and flowers to make a powerful potion. However, her activity was rudely interrupted by a",
+    villain, "who tried to hurt her. It was named", enemy)
+print()
+print("Lol anyway, it didn't really matter because", name,
+      "was powerful and had the power of", superpower)
+print()
+print("Moreover, her", cape, "cape was also helpful and her", animal,
+      "had the ability to fight off strangers")
+print()
+print("In the end, everyone lived happily ever after!")
